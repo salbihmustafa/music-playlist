31f87742f170ca7be00a8e1d486637d8e9f48dfd
Salbih Mustafa <salbihdmustafa@gmail.com>
2021-10-02 17:52:26 -0500

    add ListView component to list out all the playlists

---
+++ src/components/ListView.vue
@@ -0,0 +1,57 @@
+<template>
+  <!-- anytime you get collection there will be an id attached, see getCollection -->
+  <div v-for="playlist in playlists" :key="playlist.id">
+    <div class="single">
+        <div class="thumbnail">
+            <img :src="playlist.coverUrl">
+        </div>
+        <div class="info">
+            <h3>{{ playlist.title }}</h3>
+            <p>Created by {{ playlist.userName }}</p>
+        </div>
+        <div class="song-number">
+            <p>{{ playlist.songs.length }}</p>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["playlists"],
+};
+</script>
+
+<style scoped>
+.single {
+    display: flex;
+    align-items: center;
+    padding: 20px;
+    border-radius: 10px;
+    background: white;
+    margin: 16px 0;
+    transition: all ease 0.2s; /* animation is smooth */
+}
+.single:hover {
+    box-shadow: 1px 2px 3px rgba(50,50,50,0.05);
+    transform: scale(1.02); /* makes it pop up */
+    transition: all ease 0.2s;
+}
+.thumbnail {
+    max-width: 100px;
+    max-height: 100px;
+    overflow: hidden;
+    border-radius: 10px;
+}
+img {
+    max-width: 150%;
+    max-height: 150%;
+    display: block;
+}
+.info {
+    margin: 0 30px; /*pushes a bit of margin from pictures*/
+}
+.song-number {
+    margin-left: auto; /*push to the left*/
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,22 @@
 <template>
   <div class="home">
-    <p>Home Page</p>
+    <p>Playlists</p>
     <div class="error" v-if="error">{{ error }}</div>
     <div v-if="documents">
-      <div v-for="doc in documents" :key="doc.userId">
-        <div>{{ doc.title }}</div>
-      </div>
+      <ListView :playlists="documents" />
     </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import ListView from '@/components/ListView';
 import getCollection from "@/composables/getCollection.js"; //get database information
 import { computed, ref } from "vue";
 
 export default {
   name: "Home",
+  components: { ListView },
   setup() {
     const { documents, error } = getCollection("playlists"); //get real data collection from database
 
